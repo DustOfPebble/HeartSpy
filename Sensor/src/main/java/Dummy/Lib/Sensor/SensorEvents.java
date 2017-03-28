7a81280e393fab92bdaf26030384b7c5713dace0
Xavier Jaffrezic <xavier@jaffrezic.net>
2017-03-28 22:17:41 +0200

    solved install problem

--- Sensor/src/main/java/Dummy/Lib/Sensor/SensorEvents.java
+++
@@ -1,9 +0,0 @@
-package Dummy.Lib.Sensor;
-
-import android.bluetooth.BluetoothDevice;
-
-public interface SensorEvents {
-        void SensorFound(BluetoothDevice Sensor);
-        void UpdateFrequency(int Frequency);
-}
-
